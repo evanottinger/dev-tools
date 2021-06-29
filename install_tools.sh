a4703e435019bab95e3d767db564c3655b5c03c8
evan.ottinger <jottinger@alkugov.com>
2021-06-29 16:26:58 -0400

    Add install tools script with VSCode and PEDA

---
+++ install_tools.sh
@@ -0,0 +1,18 @@
+#!/bin/bash/
+
+# Description: A script to install tools for exploit development
+# on Debian-based Linux distributions. 
+#
+# Author: Evan Ottinger -- @OttySec
+
+# Install VSCode
+echo "[>] Installing VSCode. . ."
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O code.deb
+apt install ./code.deb
+echo "[+] VSCode installed!"
+
+# Install PEDA
+echo "[>] Installing PEDA. . ."
+git clone https://github.com/longld/peda.git /opt/peda
+echo "source /opt/peda/peda.py" >> ~/.gdbinit
+echo "[+] PEDA installed!"
